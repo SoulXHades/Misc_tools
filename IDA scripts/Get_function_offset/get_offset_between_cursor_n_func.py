9423ff23ed23e07161c7b7a256e53b12a4aa6552
LameCarrot <haojie_34@hotmail.com>
2022-11-08 13:52:49 +0800

    Update get_offset_between_cursor_n_func.py

--- IDA scripts/Get_function_offset/get_offset_between_cursor_n_func.py
+++ IDA scripts/Get_function_offset/get_offset_between_cursor_n_func.py
@@ -1,3 +1,9 @@
-# Get current cursor's address
-ea = get_screen_ea()
-print(get_func_off_str(ea))
+import idaapi
+import ida_funcs
+
+ea = get_screen_ea()    # Get cursor address
+func = idaapi.get_func(ea)
+func_offset = ea - func.start_ea
+func_name = ida_funcs.get_func_name(ea)
+print("Offset:")
+print(func_name + "+" + hex(func_offset))
