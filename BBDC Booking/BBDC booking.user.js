b1e7d69556d6eabf73f74f22821d47173f4fc1c9
LameCarrot <haojie_34@hotmail.com>
2023-07-06 20:44:09 +0800

    MISC
    - v1.0.2
    - Modified code to check it overlay loading animation DIV element has completed (opacity=0) before checking if there are slots. This is better than wait for 12sec before checking for slots as it is faster.
    - Updated README.

--- BBDC Booking/BBDC booking.user.js
+++ BBDC Booking/BBDC booking.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        BBDC booking
 // @author      lamecarrot
-// @version    	1.0.1
+// @version    	1.0.2
 // @namespace   https://lamecarrot.wordpress.com/
 // @description Notify user via browser alert if there are slots available for booking.
 // @match		*://*.booking.bbdc.sg/*
@@ -11,11 +11,17 @@
 (function() {
     'use strict';
 
+    // Check if we are at the booking page. If not, don't execute the remaining script.
+    if (window.location.href.indexOf("/booking/chooseSlot") < 0) {
+        return
+    }
+
     // Request permission for notifications
     Notification.requestPermission();
-    // Default reload every 25 seconds
-    var reload_duration = 25*1000;
+    // Default reload in 5 sec else too fast and might actually spam the server
+    var reload_duration = 5*1000;
     var reloadTimeoutId;
+    var checkSlotsAndNotifyTimeoutId;
 
     // Reload the page to see changes
     function reloadPage() {
@@ -29,24 +35,48 @@
         });
     }
 
-    // Check for the div element and send notification if it has child elements
-    function checkAndNotify() {
+    // Check for the div element and send notification if it has child elements which means have slots for booking
+    function checkSlotsAndNotify() {
+        clearTimeout(checkSlotsAndNotifyTimeoutId); // Clear the previous timeout so won't call checkSlotsAndNotify() again
         var divElement = document.querySelector('.dateList.dateList-web.d-none.d-md-flex');
         // If True means have button(s) that usually show "Jan", "Feb", etc. Months that have slot(s).
         if (divElement && divElement.children.length > 0) {
             showNotification();
             // If have slot, reset & increase reload interval to 1 min so that user have time to off the script if there are actually slots and user wants to book
             reload_duration = 60*1000;
-            clearTimeout(reloadTimeoutId); // Clear the previous timeout
             reloadTimeoutId = setTimeout(reloadPage, reload_duration); // Set the new timeout to reload the page
+        }
+        else {
+            // Reload since there are no slots available
+            reloadTimeoutId = setTimeout(reloadPage, reload_duration);
         }
     }
 
-    // Do all these only if the user is in the booking page
-    if (window.location.href.indexOf("/booking/chooseSlot") > -1) {
-        // Reload every 25 seconds
-        reloadTimeoutId = setTimeout(reloadPage, reload_duration);
-        // Delay the check by 12 seconds after page load
-        setTimeout(checkAndNotify, 12000);
+    // Check for the overlay div attribute and trigger notification if it meets the criteria
+    function checkOpacityAndNotify() {
+        var divElement = document.querySelector('.v-overlay__scrim');
+        // True means the overlay is gone and the page has loaded finish. The overlay loading animation is from vue.js while waiting for data on available slots to load from the server
+        if (divElement && divElement.style.opacity === '0') {
+            // Page loaded finish. We can now check if there are any slots for booking
+            checkSlotsAndNotify();
+        }
     }
+
+    // Create a mutation observer to detect attribute changes in the overlay div element
+    var observer_4_overlay = new MutationObserver(function(mutationsList) {
+        for (var mutation of mutationsList) {
+            if (mutation.target.classList.contains('v-overlay__scrim')) {
+                checkOpacityAndNotify();
+            }
+        }
+    });
+    // Start observing attribute modifications in the div overlay element
+    observer_4_overlay.observe(document, {
+        attributes: true,
+        attributeFilter: ['style'],
+        subtree: true
+    });
+
+    // Delay the check by 12 seconds after page load (Backup as sometimes the overlay is too fast for the UserScript to note the initial value of the overlay's opacity to compare the changes
+    checkSlotsAndNotifyTimeoutId = setTimeout(checkSlotsAndNotify, 12000);
 })();
--- BBDC Booking/README.md
+++ BBDC Booking/README.md
@@ -1,5 +1,5 @@
 # The script
-This UserScript script was created for Bukit Batok Driving Center (BBDC)'s booking session especially practical booking. This script will notify you if there is an available slot to be booked. Currently, this script has very basic features which will inform you via your browser notification if any slots are available. I might increase the number of features if I feel motivated.
+This UserScript script was created for Bukit Batok Driving Center (BBDC)'s booking session especially practical booking. This script will notify you if there is an available slot to be booked. Currently, this script has very basic features which will inform you via your browser notification if any slots are available. I might increase the number of features if I feel motivated to code.
 
 # Requirement
 As this is a UserScript, it needs browser extension such as TamperMonkey, https://www.tampermonkey.net/, to execute it. TamperMonkey is available in the extension store for both Firefox and Google Chrome.
@@ -18,3 +18,4 @@ As this is a UserScript, it needs browser extension such as TamperMonkey, https:
 4. Once enabled, refresh the page by pressing F5.
 5. In the left end of your URL search bar, press the pop-up bubble and allow notification.
 6. If there are slots available, notification should appear on the right on your computer.
+7. Disable the script by clicking on "Enabled" to book any slots you want to prevent the script from refreshing.
