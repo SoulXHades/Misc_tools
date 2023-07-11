1be214b9784ffae178ea468df00d1bc3470648cb
LameCarrot <haojie_34@hotmail.com>
2023-07-11 22:08:15 +0800

    MISC
    - Updated BBDC booking script
    - v1.1.0
    - Allows user to select the date range to be notified of available slots. (Only months for now)
    - Notify user the month'year available that has slot(s)
    - Updated README on the new features

--- BBDC Booking/BBDC booking.user.js
+++ BBDC Booking/BBDC booking.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        BBDC booking
 // @author      lamecarrot
-// @version    	1.0.2
+// @version    	1.1.0
 // @namespace   https://lamecarrot.wordpress.com/
-// @description Notify user via browser alert if there are slots available for booking.
+// @description Notify user via browser alert if there are slots available for booking. User maybe choose the date range they are interested in.
 // @match		*://*.booking.bbdc.sg/*
 // ==/UserScript==
 
@@ -19,7 +19,8 @@
     // Request permission for notifications
     Notification.requestPermission();
     // Default reload in 5 sec else too fast and might actually spam the server
-    var reload_duration = 5*1000;
+    const MIN_RELOAD_SEC = 5;
+    var reload_duration = MIN_RELOAD_SEC*1000;
     var reloadTimeoutId;
     var checkSlotsAndNotifyTimeoutId;
 
@@ -28,20 +29,107 @@
         location.reload();
     }
 
+    // Create or restore user preference
+    function createOrRestoreUserPreference() {
+        var dateRangeDiv = document.createElement('div');
+        dateRangeDiv.id = 'userPreference';
+        dateRangeDiv.style.background = '#e0e0e0';
+        dateRangeDiv.style.padding = '20px';
+        dateRangeDiv.innerHTML = `
+            <label for="start">Start Date:</label>
+            <input type="date" id="start" name="start">
+            <label for="end">End Date:</label>
+            <input type="date" id="end" name="end">
+            <label for="end">Refresh duration in seconds:</label>
+            <input type="number" id="refreshduration" name="refreshduration" min="5" value=5 style="width: 5em">
+            <button id="save">Save</button>
+        `;
+
+        // To place this at the top of the page, we will need to find the right element without page's own CSS messes things up
+        var containerDiv = document.querySelector('.container.container--fluid');
+        if (containerDiv) {
+            containerDiv.insertBefore(dateRangeDiv, containerDiv.firstChild);
+        }
+
+        var saveButton = document.getElementById('save');
+        saveButton.addEventListener('click', saveDateRange);
+
+        // Retrieve and populate the saved date range
+        var savedData = localStorage.getItem('user_preference');
+        if (savedData) {
+            var userPreference = JSON.parse(savedData);
+            var startDateInput = document.getElementById('start');
+            var endDateInput = document.getElementById('end');
+            var refreshDurationInput = document.getElementById('refreshduration');
+            startDateInput.value = userPreference.startDate;
+            endDateInput.value = userPreference.endDate;
+            refreshDurationInput.value = parseInt(userPreference.refreshDuration);
+            // Set the previously saved refresh duration
+            reload_duration = parseInt(userPreference.refreshDuration) * 1000;
+        }
+    }
+
+    // Save the selected date range
+    function saveDateRange() {
+        var startDateInput = document.getElementById('start');
+        var endDateInput = document.getElementById('end');
+        var refreshDurationInput = document.getElementById('refreshduration');
+
+        if (parseInt(refreshDurationInput.value) < 5) refreshDurationInput.value = MIN_RELOAD_SEC;
+
+        var userPreference = {
+            startDate: startDateInput.value,
+            endDate: endDateInput.value,
+            refreshDuration: parseInt(refreshDurationInput.value)
+        };
+        // Save to localstorage
+        localStorage.setItem('user_preference', JSON.stringify(userPreference));
+        // Reset the refresh timeout and set based on what user just set
+        reload_duration = parseInt(refreshDurationInput.value) * 1000;
+        clearTimeout(reloadTimeoutId);
+        reloadTimeoutId = setTimeout(reloadPage, reload_duration);
+    }
+
     // Show notification
-    function showNotification() {
+    function showNotification(available_months) {
+        var notificationBody = 'Months with slots: ' + available_months.join('\n');
         var notification = new Notification('BBDC booking', {
-            body: 'Have BBDC practical slot!!!'
+            body: notificationBody
         });
+    }
+
+    // Helper function to get the month index based on month name
+    function getMonthIndex(month) {
+        var monthNames = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+        return monthNames.indexOf(month);
+    }
+
+    // Parse the date from button text (assuming it follows the format "Month Year")
+    function parseButtonDate(buttonText) {
+        var parts = buttonText.split("'");
+        var month = parts[0];
+        var year = "20" + parts[1];
+        return new Date(year, getMonthIndex(month));
     }
 
     // Check for the div element and send notification if it has child elements which means have slots for booking
     function checkSlotsAndNotify() {
+        var available_months = [];
+        var userPreference = JSON.parse(localStorage.getItem('user_preference'));
         clearTimeout(checkSlotsAndNotifyTimeoutId); // Clear the previous timeout so won't call checkSlotsAndNotify() again
         var divElement = document.querySelector('.dateList.dateList-web.d-none.d-md-flex');
-        // If True means have button(s) that usually show "Jan", "Feb", etc. Months that have slot(s).
+        // If True means have button(s) that usually show "Jan'23", "Feb'23", etc. Months that have slot(s).
         if (divElement && divElement.children.length > 0) {
-            showNotification();
+            // Get the months that has slots available from the buttons
+            divElement.children.forEach(function(month_button) {
+                var buttonText = month_button.querySelector('.v-btn__content').innerHTML.trim();
+                available_months.push(buttonText);
+            });
+            var notificationTexts = available_months.filter(function(buttonText) {
+                var buttonDate = parseButtonDate(buttonText);
+                return buttonDate >= userPreference.startDate && buttonDate <= userPreference.endDate;
+            });
+            showNotification(available_months);
             // If have slot, reset & increase reload interval to 1 min so that user have time to off the script if there are actually slots and user wants to book
             reload_duration = 60*1000;
             reloadTimeoutId = setTimeout(reloadPage, reload_duration); // Set the new timeout to reload the page
@@ -77,6 +165,8 @@
         subtree: true
     });
 
+    // Create user preference DIV on page load
+    createOrRestoreUserPreference();
     // Delay the check by 12 seconds after page load (Backup as sometimes the overlay is too fast for the UserScript to note the initial value of the overlay's opacity to compare the changes
     checkSlotsAndNotifyTimeoutId = setTimeout(checkSlotsAndNotify, 12000);
 })();
--- BBDC Booking/README.md
+++ BBDC Booking/README.md
@@ -1,21 +1,29 @@
 # The script
 This UserScript script was created for Bukit Batok Driving Center (BBDC)'s booking session especially practical booking. This script will notify you if there is an available slot to be booked. Currently, this script has very basic features which will inform you via your browser notification if any slots are available. I might increase the number of features if I feel motivated to code.
+<img src="README_IMAGES/overview.png">
 
 # Requirement
 As this is a UserScript, it needs browser extension such as TamperMonkey, https://www.tampermonkey.net/, to execute it. TamperMonkey is available in the extension store for both Firefox and Google Chrome.
 
 # Features
 * Notify you if there are any slots available in the booking page.
+* Choose the date range to look out for slots
+   * Only filter by months for now
 
 # Usage
 1. Install TamperMonkey from your browser extension store.
 2. Add this script into TamperMonkey and enable it if need to.
    * Check if the script is detected
-   <img src="README_IMAGES/tampermonkey_script_enabled.png" width="170">
+   
+   <p align="center"><img src="README_IMAGES/tampermonkey_script_enabled.png" width="170"></p>
 3. Go to https://booking.bbdc.sg/#/booking/chooseSlot?courseType=3C&insInstructorId=&instructorType=
    * Make sure you are in the calendar booking page before enabling this script.
-    <img src="README_IMAGES/bbdc_calenderpage.png" width="400">
+
+   <p align="center"><img src="README_IMAGES/bbdc_calenderpage.png" width="400"></p>
 4. Once enabled, refresh the page by pressing F5.
 5. In the left end of your URL search bar, press the pop-up bubble and allow notification.
 6. If there are slots available, notification should appear on the right on your computer.
 7. Disable the script by clicking on "Enabled" to book any slots you want to prevent the script from refreshing.
+8. You may select the date range that you would like to be notified of any available slots.
+
+<p align="center"><img src="README_IMAGES/userPreference.png" width="400"></p>
